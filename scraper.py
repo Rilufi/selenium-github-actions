b7819d6b3bb633ea7786798d0b2c34996173b1d1
Yuri <48300685+Rilufi@users.noreply.github.com>
2022-07-25 15:57:16 -0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -3,6 +3,8 @@ from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.core.utils import ChromeType
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+from time import sleep
+import os
 
 chrome_service = Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
 
@@ -21,5 +23,14 @@ for option in options:
 
 driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
 
-driver.get('http://nytimes.com')
-print(driver.title)
+driver1.get(os.environ["VIDEO_LINK"])
+driver2.get(os.environ["VIDEO_LINK"])
+driver3.get(os.environ["VIDEO_LINK"])
+#i have used siddharth 1,2,3  so it will open 3 tabs 
+#if you want to add more tabs you can also do it 
+
+while True:
+    sleep(18)#sleep 18 meanes after 18 seconds page will refresh
+    driver1.refresh()
+    driver2.refresh()
+    driver3.refresh()
