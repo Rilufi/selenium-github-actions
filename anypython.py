5a517e99f1123d035a51e50c401d2b241863d73f
Yuri <xamexavu@hotmail.com>
2023-11-06 12:22:08 -0800

    Update anypython.py

--- anypython.py
+++ anypython.py
@@ -33,29 +33,17 @@ class TaskerBot():
 
     def login(self):
         self.driver.get('https://www.pythonanywhere.com/')
-
-#        sleep(2)
-
-#        cookie = self.driver.find_element("xpath",'//*[@id="onetrust-accept-btn-handler"]')
-#        cookie.click()
-
         sleep(2)
 
-        fb_btn = self.driver.find_element("xpath",'/html/body/div[1]/nav[1]/div/ul/li[6]/a')
+        fb_btn = self.driver.find_element(By.CLASS_NAME,'login_link')
         fb_btn.click()
-
         email_in = self.driver.find_element("xpath",'//*[@id="id_auth-username"]')
         email_in.send_keys(username)
-
         pw_in = self.driver.find_element("xpath",'//*[@id="id_auth-password"]')
         pw_in.send_keys(password)
-
         login_btn = self.driver.find_element("xpath",'//*[@id="id_next"]')
         login_btn.click()
-
-        sleep(15)
-
-
+        sleep(5)
 
     def tasker(self):
         task_btn = self.driver.find_element("xpath",'//*[@id="id_tasks_link"]')
@@ -63,8 +51,9 @@ class TaskerBot():
         sleep(2)
         extend_btn = self.driver.find_element("xpath",'//*[@id="id_scheduled_tasks_table"]/div/div/table/tbody/tr/td[6]/button[4]')
         extend_btn.click()
-
-
+        sleep(2)
+        remain_time = self.driver.find_element("xpath",'/html/body/div[1]/div[2]/div[2]/div[3]/div/div/table/tbody/tr/td[5]')
+        print('Remaining Time:', remain_time.text)
 
 bot = TaskerBot()
 bot.login()
