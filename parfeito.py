d7ab09482189d89d5a36a5359ed775de979831cb
Yuri <xamexavu@hotmail.com>
2023-11-30 12:53:58 -0300

    Update parfeito.py

--- parfeito.py
+++ parfeito.py
@@ -25,47 +25,50 @@ options = [
 for option in options:
     chrome_options.add_argument(option)
 
-class TinderBot():
+class ParPerfeitoBot():
     def __init__(self):
-        options = Options()
-        options.headless = True
         self.driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
-        
+
     def login(self):
         self.driver.get('https://www.parperfeito.com.br/')
 
         sleep(2)
 
-        cookie = self.driver.find_element("xpath",'//*[@id="onetrust-accept-btn-handler"]')
-        cookie.click()
+        # Aceitar cookies
+        cookie_btn = self.driver.find_element(By.ID, 'onetrust-accept-btn-handler')
+        cookie_btn.click()
 
         sleep(2)
 
-        fb_btn = self.driver.find_element("xpath",'//*[@id="__next"]/div/div/div/button/span')
+        # Clicar no botão de login com Facebook
+        fb_btn = self.driver.find_element(By.XPATH, '//*[@id="__next"]/div/div/div/button/span')
         fb_btn.click()
 
-        email_in = self.driver.find_element("xpath",'//*[@id="email"]')
+        email_in = self.driver.find_element(By.ID, 'email')
         email_in.send_keys(username)
 
-        pw_in = self.driver.find_element("xpath",'//*[@id="password"]')
+        pw_in = self.driver.find_element(By.ID, 'password')
         pw_in.send_keys(password)
 
-        login_btn = self.driver.find_element("xpath",'//*[@id="mainContent"]/form/div/div[1]/div[5]/button/span')
+        # Clicar no botão de login
+        login_btn = self.driver.find_element(By.XPATH, '//*[@id="mainContent"]/form/div/div[1]/div[5]/button/span')
         login_btn.click()
 
         sleep(15)
 
-
-
     def like(self):
-        like_btn = self.driver.find_element("xpath",'//*[@id="mainContent"]/div[2]/section/div[1]/div/div[4]/span/button/span/div[2]')
+        # Clicar no botão de curtir
+        like_btn = self.driver.find_element(By.XPATH, '//*[@id="mainContent"]/div[2]/section/div[1]/div/div[4]/span/button/span/div[2]')
         like_btn.click()
+
     def like2(self):
-        like_btn2 = self.driver.find_element("xpath",'//*[@id="mainContent"]/div[2]/section/div[1]/div/div[3]/span/button')
+        # Clicar no segundo botão de curtir
+        like_btn2 = self.driver.find_element(By.XPATH, '//*[@id="mainContent"]/div[2]/section/div[1]/div/div[3]/span/button')
         like_btn2.click()
 
     def dislike(self):
-        dislike_btn = self.driver.find_element("xpath",'//*[@id="mainContent"]/div[2]/section/div[1]/div/div[3]/div[2]/button/span/span[2]')
+        # Clicar no botão de descurtir
+        dislike_btn = self.driver.find_element(By.XPATH, '//*[@id="mainContent"]/div[2]/section/div[1]/div/div[3]/div[2]/button/span/span[2]')
         dislike_btn.click()
 
     def auto_swipe(self):
@@ -83,22 +86,19 @@ class TinderBot():
                         self.close_match()
 
     def close_popup(self):
-        popup_3 = self.driver.find_element("xpath",'//*[@id="mainContent"]/div[2]/section/div[1]/div/div[3]/div/div[2]/div/button')
+        # Clicar no botão para fechar o popup
+        popup_3 = self.driver.find_element(By.XPATH, '//*[@id="mainContent"]/div[2]/section/div[1]/div/div[3]/div/div[2]/div/button')
         popup_3.click()
 
     def close_match(self):
-        match_popup = self.driver.find_element("xpath",'//*[@id="mainContent"]/div[2]/section/div[1]/div/div[4]/div/div[2]/div/button')
+        # Clicar no botão para fechar a janela de match
+        match_popup = self.driver.find_element(By.XPATH, '//*[@id="mainContent"]/div[2]/section/div[1]/div/div[4]/div/div[2]/div/button')
         match_popup.click()
 
-
-bot = TinderBot()
+bot = ParPerfeitoBot()
 bot.login()
 
 try:
     bot.auto_swipe()
 except:
-    ask = 's' #input("Deseja continuar? ")
-    if ask != 'n':
-        bot.auto_swipe()
-    else:
-        pass
+    print("Nope")
