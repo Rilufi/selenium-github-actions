b5fab8cd8c6642c770c69e57663adc3e391b3a39
Yuri <xamexavu@hotmail.com>
2023-11-30 14:23:40 -0300

    Update parfeito.py

--- parfeito.py
+++ parfeito.py
@@ -31,9 +31,11 @@ class ParPerfeitoBot():
     def __init__(self):
         self.driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
 
-    def login(self):
+def login(self):
         self.driver.get('https://www.parperfeito.com.br/')
-
+        self.driver.implicitly_wait(10)  # Aguarde até 10 segundos por qualquer elemento a ser encontrado
+        print(self.driver.page_source)  # Exibir HTML da página no console para debug
+        email_in = self.driver.find_element(By.ID, 'email')
         sleep(2)
 
         # Aceitar cookies
