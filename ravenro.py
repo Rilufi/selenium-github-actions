ec6b043154e8d74ea38ed7466907a16f0c939184
Yuri <xamexavu@hotmail.com>
2024-06-03 15:40:43 -0300

    Update ravenro.py

--- ravenro.py
+++ ravenro.py
@@ -1,14 +1,14 @@
 import os
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
-#from webdriver_manager.core.utils import ChromeType
 from webdriver_manager.core.os_manager import ChromeType
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.keys import Keys
-from time import sleep, time
-from datetime import datetime
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException, NoSuchElementException, NoAlertPresentException
+from time import sleep
 
 # Lista de credenciais
 credenciais = [
@@ -35,50 +35,50 @@ for option in options:
 
 class TaskerBot():
     def __init__(self):
-        options = Options()
-        options.headless = True
         self.driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
+        self.wait = WebDriverWait(self.driver, 15)
 
     def login(self, usuario, senha):
         self.driver.get('https://cp.ravenro.com.br/')
         sleep(2)
-        email_in = self.wait.until(EC.presence_of_element_located((By.XPATH, '//input[@placeholder="Login"]')))
-        email_in.send_keys(usuario)
-        pw_in = self.driver.find_element(By.XPATH, '//input[@placeholder="Senha"]')
-        pw_in.send_keys(senha)
-        login_btn = self.driver.find_element(By.XPATH, '//button[contains(@class, "baTaHaVg0")]')
-        login_btn.click()
-        sleep(5)
+        try:
+            email_in = self.wait.until(EC.presence_of_element_located((By.XPATH, '//input[@placeholder="Login"]')))
+            email_in.send_keys(usuario)
+            pw_in = self.driver.find_element(By.XPATH, '//input[@placeholder="Senha"]')
+            pw_in.send_keys(senha)
+            login_btn = self.driver.find_element(By.XPATH, '//button[contains(@class, "baTaHaVg0")]')
+            login_btn.click()
+            sleep(5)
+        except TimeoutException as e:
+            print(f"Erro ao localizar elementos de login: {e}")
 
     def voter1(self):
         self.driver.get('https://cp.ravenro.com.br/votar')
         sleep(5)
         try:
-            btn = self.wait.until(EC.element_to_be_clickable(By.XPATH,'//button[@class="clickable-element bubble-element Button baTaHxaE"]'))
+            btn = self.wait.until(EC.element_to_be_clickable((By.XPATH, '//button[@class="clickable-element bubble-element Button baTaHxaE"]')))
             btn.click()
             sleep(2)
-        except:
+        except (TimeoutException, NoSuchElementException):
             time_elements = self.driver.find_elements(By.XPATH, '//div[contains(text(), "Aguarde:") and contains(@class, "bubble-element Text")]')
-                for element in time_elements:
-                    print(f"Tempo restante para votar: {element.text}")
+            for element in time_elements:
+                print(f"Tempo restante para votar: {element.text}")
             sleep(2)
 
-    
     def voter2(self):
         self.driver.get('https://cp.ravenro.com.br/votar')
         sleep(5)
         try:
-            btn = self.wait.until(EC.element_to_be_clickable(By.XPATH,'//button[@class="clickable-element bubble-element Button baTaIaCh"]'))
+            btn = self.wait.until(EC.element_to_be_clickable((By.XPATH, '//button[@class="clickable-element bubble-element Button baTaIaCh"]')))
             btn.click()
             sleep(2)
-        except:
+        except (TimeoutException, NoSuchElementException):
             time_elements = self.driver.find_elements(By.XPATH, '//div[contains(text(), "Aguarde:") and contains(@class, "bubble-element Text")]')
-                for element in time_elements:
-                    print(f"Tempo restante para votar: {element.text}")
+            for element in time_elements:
+                print(f"Tempo restante para votar: {element.text}")
             sleep(2)
 
     def logoff(self):
-        # Voltar para a página inicial
         self.driver.get('https://cp.ravenro.com.br/inicio')
         try:
             logoff_btn = self.wait.until(EC.element_to_be_clickable((By.XPATH, '//button[contains(@class, "logoff-btn")]')))
